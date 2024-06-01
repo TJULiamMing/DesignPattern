1a54ffd2f123fcd81da44789decbf9690fb693ef
TJULiamMing <lupeiming2022@gmail.com>
2024-06-01 23:18:42 +0800

    生成器模式注释

--- CreationalPatterns/builder/test/main.go
+++ CreationalPatterns/builder/test/main.go
@@ -9,10 +9,13 @@ import (
 // 下面封装了villa的构建过程
 // 如需拓展多个产品，只要实现相应builder接口（例如apartment.go）
 func main() {
+	//获取指定产品生成器
 	villabuilder := builder.Villabuilder{}
 
+	//由director封装构造过程
 	director := builder.NewDirector(&villabuilder)
 	director.BuildHouse()
 
+	//返回构造结果
 	fmt.Println(villabuilder.GetHouse())
 }
